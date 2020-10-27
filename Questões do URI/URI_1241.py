6bfd53985209e99729a645884cb4c1fc22c1697c
CauanyRodrigues01 <cauanyrodrigues01@gmail.com>
2020-10-26 22:49:19 -0300

    26/10/20

--- Questões do URI/URI_1241.py
+++ Questões do URI/URI_1241.py
@@ -1,8 +1,7 @@
-testes = int(input())
-for i in range(testes):
+loops = int(input())
+for i in range(loops):
     num1, num2 = [str(i) for i in input().split()]
-    parte_comparar = num2[-len(num2):]
-    print(parte_comparar)
+    parte_comparar = num1[-len(num2):]
     if num2 in parte_comparar:
         print("encaixa")
     else:
